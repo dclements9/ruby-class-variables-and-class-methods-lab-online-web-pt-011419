58fc7c411792076816db83dba9a5e74e53f67c7f
Dylan Clements <dclements@protonmail.com>
2019-02-06 20:55:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
     def genre_count
       genre_count_hash = {}
         @@genres.each do |genre|
-          if genre_count_hash.include?(genre)
+          if genre_count_hash[genre]
             genre_count_hash[genre] += 1
           else
             genre_count_hash[genre] = 1
