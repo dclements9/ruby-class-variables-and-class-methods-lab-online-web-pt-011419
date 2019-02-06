f9ef0b51de04ed4c3e962767917188d9cb65d1ac
Dylan Clements <dclements@protonmail.com>
2019-02-06 21:04:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,11 +20,9 @@ class Song
     def self.genres
       @@genres.uniq
     end
-
     def self.artists
       @@artists.uniq
     end
-
     def self.genre_count
       genre_count_hash = {}
         @@genres.each do |genre|
