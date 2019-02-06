0f746ceda3e72536412571c48ebcf8d3d5ce9259
Dylan Clements <dclements@protonmail.com>
2019-02-06 20:19:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,20 @@
 class Song
   @@count = 0
   @@genre = []
+  @@artist = []
 
   attr_accessor :name, :artist, :genre
     def initialize(name, artist, genre)
         @name = name
         @artist = artist
         @genre = genre
-      @@count += 1
-      @@genre << genre
+        @@count += 1
+        unless @@genre.include?(genre)
+          @@genre << genre
+        end
+        unless @@artist.include?(artist)
+          @@artist << artist
+        end
     end
     def self.count
       @@count
@@ -16,4 +22,6 @@ class Song
     def self.genre
       @@genre
     end
+
+
 end
